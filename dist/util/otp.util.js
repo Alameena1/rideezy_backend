15ea5414014c7415a9b63ed9421c6dce08f478eb
alameen <alameena8841@gmail.com>
2025-04-05 18:16:59 +0530

    Remove dist folder from repo and add to .gitignore

--- dist/util/otp.util.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class OTPUtil {
-    static generateOTP() {
-        return Math.floor(100000 + Math.random() * 900000).toString();
-    }
-}
-exports.default = OTPUtil;
